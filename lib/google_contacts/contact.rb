4f77c6f6c9c6d05256bedbabf2655a89cef423a0
Nick Malcolm <nick@revert.io>
2015-06-05 17:14:40 +1200

    Initialize Contact with an entry (not the entire response from google).

--- lib/google_contacts/contact.rb
+++ lib/google_contacts/contact.rb
@@ -1,16 +1,4 @@
-class GoogleContacts::Contact
-
-  # Public: initializes a Contact using Google Contacts API JSON data
-  def initialize(json={})
-    @json = json
-    @entry = GoogleContacts::Attribute.new(@json["entry"])
-  end
-
-  # Send any missing methods to our magical entry
-  def method_missing(method, *args, &block)
-    @entry.send(method, *args, &block)
-  rescue NoMethodError => error
-    super
-  end
-
+# Public: a GoogleContact.
+# Should be initialized with a JSON "entry"
+class GoogleContacts::Contact < GoogleContacts::Atribute
 end
